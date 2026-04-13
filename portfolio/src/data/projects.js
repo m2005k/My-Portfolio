70495c1177701aabe24f336f729db9b72f3b16af
Mandeep kaur <samritis548@icloud.com>
2026-04-13 22:34:15 +0530

    final update

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,21 +1,21 @@
 export const projectData = [
   {
     title: "Wanderlust",
-    description: "Built using React, CSS and JavaScript",
+    description: "Built using HTML, CSS and JavaScript, with a Node.js and Express backend, and MongoDB for data storage.",
     live: "https://wanderlust-1-fesu.onrender.com/listings",
     github: "https://github.com/m2005k/Wanderlust.git",
     image: "/assets/airbnb.jpeg"
   },
   {
     title: "Simon Says Game",
-    description: "Task manager with local storage",
+    description: "Built using basic HTML & CSS for structure and styling, and JavaScript for game logic .",
     live: "https://simon-says-game-blush-beta.vercel.app",
     github: "https://github.com/m2005k/Simon-Says-Game.git",
     image: "/assets/Simon.jpg"
   },
   {
     title: "Netflix Clone",
-    description: "Uses API to fetch real-time data",
+    description: "Built using HTML & CSS to showcase my front-end development skills and create a visually appealing user interface.",
     live: "https://movie-streaming-app-ui-one.vercel.app",
     github: "https://github.com/m2005k/Movie-Streaming-App-UI",
     image: "/assets/netflix.jpg"
